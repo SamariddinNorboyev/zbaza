398ad08c849aeb2caeda2521539138f83cafcb16
Samariddin <139744184+SamariddinNorboyev@users.noreply.github.com>
2025-07-05 17:56:59 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -74,19 +74,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'zbaza',
-#         'USER': 'postgres',
-#         'PASSWORD': '00000191',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
